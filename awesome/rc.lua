1d78369f434a2b75f444e6af8a641a3dd25961e8
W-Dominic <dom0_o@protonmail.com>
2023-04-18 16:39:54 -0400

    collision plugin

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,3 +1,8 @@
+-- START PLUGINS
+require("collision")()
+
+-- END PLUGINS
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
