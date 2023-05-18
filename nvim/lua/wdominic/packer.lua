d039e5e866a3a17da4b0f0e6eb312cc6e654a77f
W-Dominic <dom0_o@protonmail.com>
2023-05-18 15:24:43 -0400

    lsp

--- nvim/lua/wdominic/packer.lua
+++ nvim/lua/wdominic/packer.lua
@@ -20,4 +20,29 @@ return require('packer').startup(function(use)
 	
 	-- Treesitter
 	use ('nvim-treesitter/nvim-treesitter', {use = ':TSUpdate'} )
+	
+	-- Harpoon
+	use ('theprimeagen/harpoon')
+
+	-- LSP zero
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
 end)
